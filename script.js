bfaaf874dc2432499d2a5df543a174de1b2b50ec
Yifan <yifanyemontpe@gmail.com>
2023-11-04 14:25:41 +0100

    Start and score behavior

--- script.js
+++ script.js
@@ -2,33 +2,56 @@ import { generateGround, updateGround } from "./ground.js"
 
 const WORLD_WIDTH = 100
 const WORLD_HEIGHT = 30
+const SPEED_SCALE_INCREASE = 0.00001
 
 const worldElement = document.querySelector('[data-world]')
-
-generateGround()
+const scoreElement = document.querySelector('[data-score')
+const startScreenElement = document.querySelector('[data-start-screen')
 
 setPixelToWorldScale()
 window.addEventListener("resize", setPixelToWorldScale)
 document.addEventListener("keydown", handleStart, { once: true })
 
 let lastTime
+let speedScale
+let score
+
 function update(time) {
   if (lastTime == null) {
     lastTime = time
+    // Scheduling and synchronizing animations 
+    // taking advantage of the browser's rendering capabilities
     window.requestAnimationFrame(update)
     return
   }
 
   const delta = time - lastTime
-  updateGround(delta, 1)
+
+  updateGround(delta, speedScale)
+  updateSpeedScale(delta)
+  updateScore(delta)
 
   lastTime = time
   window.requestAnimationFrame(update)
 }
 
-// Scheduling and synchronizing animations 
-// taking advantage of the browser's rendering capabilities
-window.requestAnimationFrame(update)
+function updateSpeedScale(delta) {
+  speedScale += delta * SPEED_SCALE_INCREASE
+}
+
+function updateScore(delta) {
+  score += delta * 0.01
+  scoreElement.textContent = Math.floor(score)
+}
+
+function handleStart() {
+  lastTime = null
+  speedScale = 1
+  score = 0
+  generateGround()
+  startScreenElement.classList.add("hide")
+  window.requestAnimationFrame(update)
+}
 
 // Scale world on screen dimensions
 function setPixelToWorldScale() {
